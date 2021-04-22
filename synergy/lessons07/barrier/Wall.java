b37435b0bc63abc225881e3462b2f2f0fc1c950d
pavka195 <81584911+pavka195@users.noreply.github.com>
2021-04-22 22:22:53 +0300

    Add files via upload

---
+++ synergy/lessons07/barrier/Wall.java
@@ -0,0 +1,26 @@
+package synergy.lessons07.barrier;
+
+import synergy.lessons07.sportsman.Sportsman;
+
+public class Wall extends Barrier{
+    private final int wallHeight;
+
+    public Wall(String name, int wallHeight) {
+        super(name);
+        this.wallHeight = wallHeight;
+    }
+
+    @Override
+    public boolean action(Sportsman sportsman) {
+        System.out.println("Высота барьера \"" + super.getName() + "\" составляет: " + this.wallHeight + " м.");
+        //Спортсмен прыгает и мы проверяем, преодолел ли он барьер
+        sportsman.jump();
+        if (this.wallHeight <= sportsman.wallHeight()) {
+            System.out.println(sportsman.nameSportsman() + " успешно преодолел барьер!");
+            return true;
+        } else {
+            System.out.println(sportsman.nameSportsman() + " не смог преодолеть барьер...");
+            return false;
+        }
+    }
+}
