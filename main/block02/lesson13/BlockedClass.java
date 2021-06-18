f38747ddc6e11c24f632c0108f23f9c371757603
pavka195 <81584911+pavka195@users.noreply.github.com>
2021-06-18 14:49:36 +0300

    Delete main/block02/lesson13 directory

--- main/block02/lesson13/BlockedClass.java
+++
@@ -1,9 +0,0 @@
-package main.block02.lesson13;
-
-public class BlockedClass {
-    @Blocked(name = "stop")
-    public String stop() {
-        String str = "Запрещено использовать метод";
-        return str;
-    }
-}
--- main/block02/lesson13/Score.java
+++
@@ -1,9 +0,0 @@
-package main.block02.lesson13;
-
-import java.math.BigDecimal;
-
-public interface Score {
-
-    void withdrawals(BigDecimal summa);
-    void topUp(BigDecimal summa);
-}
