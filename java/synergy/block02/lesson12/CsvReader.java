47b2e23b3efead3761a62197b444d8d6220ec7a6
pavka195 <81584911+pavka195@users.noreply.github.com>
2021-06-18 14:00:11 +0300

    Delete java/synergy/block02/lesson12 directory

--- java/synergy/block02/lesson12/CsvReader.java
+++
@@ -1,7 +0,0 @@
-package main.java.synergy.block02.lesson12;
-
-public class CsvReader {
-    public static String getContent(String fileName) {
-        return "";
-    }
-}
--- java/synergy/block02/lesson12/CustomRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package main.java.synergy.block02.lesson12;
-
-public class CustomRuntimeException extends RuntimeException {
-
-    public CustomRuntimeException() {
-        super("Custom exception happens during execution");
-    }
-}
