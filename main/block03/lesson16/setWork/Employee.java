94e07156c9de559bde7ab64b7516c55b610242a7
pavka195 <81584911+pavka195@users.noreply.github.com>
2021-07-06 21:49:09 +0300

    Add files via upload

---
+++ main/block03/lesson16/setWork/Employee.java
@@ -0,0 +1,12 @@
+package main.block03.lesson16.setWork;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class Employee {
+    private String fio;
+    private BigDecimal workAge;
+}
