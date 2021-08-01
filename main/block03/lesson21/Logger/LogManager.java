a36080c130a25e00d59dbea439ac8d414bd1cae1
pavka195 <81584911+pavka195@users.noreply.github.com>
2021-08-01 16:19:58 +0300

    Add files via upload

---
+++ main/block03/lesson21/Logger/LogManager.java
@@ -0,0 +1,45 @@
+package main.block03.lesson21.Logger;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+@Data
+@NoArgsConstructor
+public class LogManager extends Thread {
+    private static final List<String> LEVEL = new ArrayList<>();
+    private String msg;
+    FileOutputStream outputStream;
+
+    {
+        try {
+            outputStream = new FileOutputStream("C:/Java/result.txt", true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static {
+        LEVEL.add("INFO");
+        LEVEL.add("ERROR");
+        LEVEL.add("WARN");
+        LEVEL.add("DEBUG");
+        LEVEL.add("TRACE");
+    }
+
+    @SneakyThrows
+    public void writeToFile() {
+        Date date = new Date();
+        Random random = new Random();
+        String str = "Дата/время: " + date + ", Уровень лога: " + LEVEL.get(random.nextInt(5)) + ", Сообщение: " + msg + "\r\n";
+        this.outputStream.write(str.getBytes(StandardCharsets.UTF_8));
+    }
+}
---
+++ main/block03/lesson21/LoggerRunner.java
@@ -0,0 +1,58 @@
+package main.block03.lesson21;
+
+import main.block03.lesson21.Logger.LogManager;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LoggerRunner {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LogManager lm = new LogManager();
+        Random random = new Random();
+
+        Runnable task = () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                lm.setMsg(Thread.currentThread().getName());
+                lm.writeToFile();
+                try {
+                    Thread.sleep(random.nextInt(5000));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            String inputFileName = "C:/Java/result.txt";
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread thread = new Thread(task);
+        Thread thread2 = new Thread(task);
+        Thread thread3 = new Thread(task);
+        thread.start();
+        thread2.start();
+        thread3.start();
+
+        Timer t = new Timer();
+        TimerTask taskTimer = new TimerTask() {
+            public void run() {
+                thread.interrupt();
+                thread2.interrupt();
+                thread3.interrupt();
+                t.cancel();
+            }
+        };
+        System.out.println("Current time: " + new Date());
+        t.schedule(taskTimer, 60000);
+    }
+}
